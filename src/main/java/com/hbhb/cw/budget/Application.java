09823c1982ba8a8bff5c93941b2d6f9164bb7725
yzc <1336507760@qq.com>
2021-01-06 20:05:37 +0800

    1.06 预算分解bug修复 1,目标与进度统计数据修复。2，归口单位，预算，项目签报添加新字段serial_num 3，修改签报生成，预算生成时同时生成serial_num

--- src/main/java/com/hbhb/cw/budget/Application.java
+++ src/main/java/com/hbhb/cw/budget/Application.java
@@ -18,6 +18,5 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-
     }
 }
